fb220236cd5069b08854d99da6b0c00aaf0810a6
Pramuditha Aravinda <aravindadp@gmail.com>
2018-06-27 19:54:16 +0530

    feat: Implement KalmanFilter Class
    Implement the prediction and update equations.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -25,6 +26,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_; //Lesson 5 Section 8
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_; // Lesson 5 Section 9
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +36,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +56,38 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  //Lesson 5 Section 14
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  float rho = sqrt(px*px+py*py);
+  float theta = atan2(py,px);
+  float ro_dot = (px*vx+py*vy)/rho;
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho,theta,ro_dot;
+
+  VectorXd y = z - z_pred;
+  if(y[1] > M_PI)
+  {
+    y[1] -= 2*M_PI;
+  }
+  else if(y[1]<(-M_PI))
+  {
+    y[1] += 2*M_PI;
+  }
+
+  //Lesson 5 Section 7
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
